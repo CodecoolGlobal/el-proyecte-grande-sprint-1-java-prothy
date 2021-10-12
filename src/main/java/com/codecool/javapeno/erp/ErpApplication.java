2885e1194030e3c08d9d51bea520093460627ba6
prothy <lukacsurgay@gmail.com>
2021-10-12 14:40:50 +0200

    feat: add CORS header config bean to main class

--- src/main/java/com/codecool/javapeno/erp/ErpApplication.java
+++ src/main/java/com/codecool/javapeno/erp/ErpApplication.java
@@ -2,7 +2,17 @@ package com.codecool.javapeno.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -13,5 +23,24 @@ public class ErpApplication {
 	}
 
 	//http://localhost:8080/swagger-ui.html
+
+
+	/**
+	 * Bean to define global CORS.
+	 *
+	 * @return
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowCredentials(true)
+						.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+			}
+		};
+	}
 
 }
